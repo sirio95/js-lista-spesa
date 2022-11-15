5542039916d91b216f5014bcf8d30784b70a5c62
Sirio <sirio.iasevoli@gmail.com>
2022-11-15 09:40:03 +0100

    esercizio

--- js/script.js
+++ js/script.js
@@ -9,29 +9,11 @@ console.log(listaSpesa);
 console.log(lista_spesa_lenght);
 
 
+
 while(i < lista_spesa_lenght){
     let element= `<label for="${listaSpesa[i]}">${listaSpesa[i]}</label><input type="checkbox" id="${listaSpesa[i]}"></br>`
     lista.innerHTML += element;
     i++;
-
-    if(i == lista_spesa_lenght -1){
-        let new_element = `<input type="text" id="nuovo_elemento"><button id="button_lista">Inserisci nuovo ingrediente alla lista</button>`;
-        spesa.innerHTML= new_element;
-        button_lista.addEventListener('click', 
-            function(){
-                var ingredienteN= document.getElementById("nuovo_elemento");
-                var nuovo_ingrediente= ingredienteN.value;
-                listaSpesa.push(nuovo_ingrediente);
-            }
-        )            
-            
-    }
-    i++;
-
-
-
-    
-    
 }
 
 
